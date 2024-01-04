4645c171a862b6b584a149fd25e6485091aae7cd
dmichaels411 <dmichaels411@gmail.com>
2024-01-04 13:59:21 -0500

    added API actions

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _dataContext;
+
+            public Handler(DataContext dataContext)
+            {
+                _dataContext = dataContext;
+            }
+
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _dataContext.Activities.FindAsync(request.Id);
+                _dataContext.Activities.Remove(activity);
+                await _dataContext.SaveChangesAsync();
+            }
+        }
+    }
+}
