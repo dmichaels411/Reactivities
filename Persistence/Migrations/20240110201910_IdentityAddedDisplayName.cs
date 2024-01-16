cf8f14e2ee423509bc2e25ed6794222ba28f99c6
dmichaels411 <dmichaels411@gmail.com>
2024-01-16 13:58:31 -0500

    added identity authentication and authorization

---
+++ Persistence/Migrations/20240110201910_IdentityAddedDisplayName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityAddedDisplayName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisokayName",
+                table: "AspNetUsers",
+                newName: "DisplayName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                newName: "DisokayName");
+        }
+    }
+}
