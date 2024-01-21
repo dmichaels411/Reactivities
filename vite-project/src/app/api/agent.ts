90853a2fd3fce1d0df86ca356317c237e81ffc10
dmichaels411 <dmichaels411@gmail.com>
2024-01-20 21:04:25 -0500

    added image upload to api

--- vite-project/src/app/api/agent.ts
+++ vite-project/src/app/api/agent.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
-import { Activity } from '../models/activity';
+import { Activity, ActivityFormValues } from '../models/activity';
 import { toast } from 'react-toastify';
 import { router } from './router/Routes';
 import { store } from '../stores/store';
